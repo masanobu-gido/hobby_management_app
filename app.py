a8f98a551bca9eedd5fd64a2c96ec728eba4c30e
masanobugido <g.nobu.0807@gmail.com>
2022-03-22 11:37:18 +0900

    test the db 2

--- app.py
+++ app.py
@@ -165,5 +165,5 @@ def delete(id):
 
 
 if __name__ == '__main__':
-    db.create_all()
+    #db.create_all()
     app.run(debug=True)
--- score.py
+++ score.py
@@ -3,10 +3,14 @@ import psycopg2 as pg
 import os
 
 def hobby_score(user_id):
+
     #connection = pg.connect("host='localhost' port=5432 dbname=hobbysql user=postgres password='mg-94371210'")
     #connection = pg.connect("host='ec2-3-222-204-187.compute-1.amazonaws.com' port=5432 dbname=davqklevog6vet user=gpwjeheomhdmfu password='afe549ec72373ac34258388ca47b4e8a18281263998b9f773860a7c8b522e2fb'", sslmode='require')
-    DATABASE_URL = 'postgres://gpwjeheomhdmfu:afe549ec72373ac34258388ca47b4e8a18281263998b9f773860a7c8b522e2fb@ec2-3-222-204-187.compute-1.amazonaws.com:5432/davqklevog6vet'
-    connection = pg.connect(DATABASE_URL)
+    DATABASE_URL = "postgres://lkmpgyrwoildlh:20528f08836974c86b49632c641133e72432643b3b92193a18b0143787632739@ec2-174-129-243-38.compute-1.amazonaws.com:5432/d2g6vau23oegqv"
+    # DATABASE_URL = 'postgres://gpwjeheomhdmfu:afe549ec72373ac34258388ca47b4e8a18281263998b9f773860a7c8b522e2fb@ec2-3-222-204-187.compute-1.amazonaws.com:5432/davqklevog6vet'
+    #DATABASE_URL = os.environ.get('DATABASE_URL')
+    print(DATABASE_URL)
+    connection = pg.connect(DATABASE_URL, sslmode='require')
     df = pd.read_sql_query('SELECT * FROM hobbes;', connection)
     df.loc[:, 'time*feeling'] = df.loc[:, 'time'] * df.loc[:, 'feeling']
     user_df = df[df["user_id"] == user_id]
@@ -29,6 +33,7 @@ def hobby_score(user_id):
     return score_dict, max_score
 
 def hobby(user_id):
+    
     connection = pg.connect("host='localhost' port=5432 dbname=hobbysql user=postgres password='mg-94371210'")
     df = pd.read_sql_query('SELECT * FROM hobbes;', connection)
     user_df = df[df["user_id"] == user_id]
@@ -36,6 +41,7 @@ def hobby(user_id):
     connection.close()
     
     return hobbies
+
 
 if __name__ == '__main__':
     hobby_score(2)
